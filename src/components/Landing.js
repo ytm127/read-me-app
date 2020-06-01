6c4e96575b1f6c12b84165f27fcbe4dc27711685
Thomas Morita <>
2020-06-01 11:05:17 -0400

    landing page pretty much done

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,31 +5,75 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const Landing = () => {
   return (
-    <div className="container">
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col s6">
+            <h1 style={{ color: "white", marginTop: "15vh" }}>
+              Read.me makes it easy to track of all the books you read
+            </h1>
+            <div className="row">
+              <div className="col">
+                <a className="waves-effect waves-light btn btn-large">
+                  Learn more
+                </a>
+              </div>
+              <div className="col">
+                <a className="waves-effect waves-light btn btn-large">
+                  <FontAwesomeIcon icon={faBook} size="1x" />
+                  &nbsp; Start reading
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col s6">
+            <div>
+              <img src={picture} alt="pic2" style={{ marginTop: "7vh" }} />
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* BELOW FOLD */}
       <div className="row">
-        <div className="col s6">
-          <h1 style={{ color: "white", marginTop: "15vh" }}>
-            Read.me makes it easy to track of all the books you read
-          </h1>
-          <div className="row">
-            <div className="col">
-              <a className="waves-effect waves-light btn btn-large">
-                Learn more
-              </a>
-            </div>
-            <div className="col">
-              <a className="waves-effect waves-light btn btn-large">
-                <FontAwesomeIcon icon={faBook} size="1x" />
-                &nbsp;  Start reading
-              </a>
+        <div style={{ marginLeft: "4vw", marginRight: "4vw" }}>
+          <div className="card" style={{ borderRadius: 10 }}>
+            <div className="card-content" style={{ padding: "5vh" }}>
+              <div className="row">
+                <div className="col s4" style={{ textAlign: "center" }}>
+                  <h5>Manage your books</h5>
+                  <div
+                    style={{ fontSize: 16, marginTop: "3vh", color: "#616161" }}
+                  >
+                    Add new books you're reading and manage collection of the
+                    books you've read in your "library"
+                  </div>
+                </div>
+                <div className="col s4" style={{ textAlign: "center" }}>
+                  <h5>Add your thoughts</h5>
+                  <div
+                    style={{ fontSize: 16, marginTop: "3vh", color: "#616161" }}
+                  >
+                    Add your thoughts and comments after, or while you're
+                    reading the book. You can also your books for future
+                    reference
+                  </div>
+                </div>
+                <div className="col s4" style={{ textAlign: "center" }}>
+                  <h5>Share with your friends</h5>
+                  <div
+                    style={{ fontSize: 16, marginTop: "3vh", color: "#616161" }}
+                  >
+                    Let your friends know what you're currently reading
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <div className="col s6">
-          <div>
-            <img src={picture} alt="pic2" style={{ marginTop: "16vh" }} />
-          </div>
-        </div>
+      </div>
+      {/* Bottom part */}
+      <div className="row">
+
       </div>
     </div>
   );
