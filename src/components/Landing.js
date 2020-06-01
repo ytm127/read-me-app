b5be89800dbeb9743455503269919a2618e3abd0
Thomas Morita <>
2020-06-01 10:19:06 -0400

    get font awesome set up

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,5 +1,7 @@
 import React from "react";
 import picture from "../pic1.png";
+import { faBook } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const Landing = () => {
   return (
@@ -7,12 +9,25 @@ export const Landing = () => {
       <div className="row">
         <div className="col s6">
           <h1 style={{ color: "white", marginTop: "15vh" }}>
-            Read.me makes it easy to track of all the books you read{" "}
+            Read.me makes it easy to track of all the books you read
           </h1>
+          <div className="row">
+            <div className="col">
+              <a className="waves-effect waves-light btn btn-large">
+                Learn more
+              </a>
+            </div>
+            <div className="col">
+              <a className="waves-effect waves-light btn btn-large">
+                <FontAwesomeIcon icon={faBook} size="1x" />
+                &nbsp;  Start reading
+              </a>
+            </div>
+          </div>
         </div>
         <div className="col s6">
           <div>
-            <img src={picture} alt="pic2" style={{marginTop:"16vh"}}/>
+            <img src={picture} alt="pic2" style={{ marginTop: "16vh" }} />
           </div>
         </div>
       </div>
